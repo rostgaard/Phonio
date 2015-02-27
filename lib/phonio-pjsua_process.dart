b4895341608167f4828e33ac8dfe74f3b2ab2b8c
Kim Rostgaard Christensen <krc@retrospekt.dk>
2015-02-27 21:12:54 +0100

    Initial commit of the PJSUA backed and version bump.

---
+++ lib/phonio-pjsua_process.dart
@@ -0,0 +1,247 @@
+part of phonio;
+
+abstract class Configuration {
+  static int Loglevel = 2;
+}
+
+
+abstract class PJSUACommand {
+  static const String REGISTER            = 'r';
+  static const String UNREGISTER          = 'u';
+  static const String HANGUP_CURRENT      = 'H';
+  static const String HANGUP_ALL          = 'h';
+  static const String _DIAL               = 'd';
+  static const String ENABLE_AUTO_ANSWER  = 'a';
+  static const String DISABLE_AUTO_ANSWER = 'm';
+  static const String ANSWER_CALL         = 'p';
+
+  static String dialString (String extension, {String server : ''})
+    => '${PJSUACommand._DIAL}sip:${extension}${server.isNotEmpty ? '@${server}' : ''}';
+}
+
+abstract class PJSUAEvent {
+  static const String READY          = '!READY';
+  static const String DIALING        = '!RINGING';
+  static const String INCOMING_CALL  = 'CALL_INCOMING';
+  static const String OUTGOING_CALL  = 'CALL_OUTGOING';
+}
+
+abstract class PJSUAResponse {
+  static const String OK    = '+OK';
+  static const String ERROR = '-ERROR';
+}
+
+
+
+//TODO: Throttle the rate of commands being sent per second, as the SIP process tends to choke on them.
+class PJSUAProcess extends SIPPhone {
+
+  static const String classname = '${libraryName}.PJSUAProcess';
+
+    Logger           log         = new Logger(PJSUAProcess.classname);
+    final String      binaryPath;
+    IO.Process       _process    = null;
+    bool             _ready      = false;
+    Queue<Completer> _readyQueue = new Queue();
+
+    int _defaultAccountID = null;
+    SIPAccount get defaultAccount {
+      if (this._defaultAccountID == null) {
+        return this._accounts.first;
+      }
+
+      return this._accounts[this._defaultAccountID];
+    }
+
+    /// The Job queue is a simple FIFO of Futures that complete in-order.
+    Queue<Completer<String>> replyQueue   = new Queue<Completer<String>>();
+
+    bool           get connected   => this._process != null;
+
+    PJSUAProcess (this.binaryPath);
+
+    Future autoAnswer(bool enabled, {SIPAccount account : null}) {
+      // Default to the first account
+      int accountID = (account != null ? account.ID : 1);
+
+      return this._subscribeAndSend((enabled ? PJSUACommand.ENABLE_AUTO_ANSWER
+                                             : PJSUACommand.DISABLE_AUTO_ANSWER));
+    }
+
+    Future hold() => new Future.error(new UnimplementedError());
+
+    Future hangup() => new Future.error(new UnimplementedError());
+
+    Future hangupSpecific(Call call) => new Future.error(new UnimplementedError());
+
+    Future hangupAll() => new Future.error(new UnimplementedError());
+
+    Future release(Call call) => new Future.error(new UnimplementedError());
+
+    Future transfer(Call destination) => new Future.error(new UnimplementedError());
+
+    Future<String> registerAccount() => this._subscribeAndSend(PJSUACommand.REGISTER);
+
+    Future<Call> originate (String extension, {SIPAccount account : null}) {
+      if (account == null) {
+        account = this.defaultAccount;
+      }
+
+      return this._subscribeAndSend
+          (PJSUACommand.dialString
+              (extension, server: this.defaultAccount.server))
+              .then ((_) => null);
+    }
+
+    Future<String> unregisterAccount() => this._subscribeAndSend(PJSUACommand.UNREGISTER);
+
+    Future<String> hangupCurrentCall() => this._subscribeAndSend(PJSUACommand.HANGUP_CURRENT);
+    Future<String> hangupAllCalls() => this._subscribeAndSend(PJSUACommand.HANGUP_ALL);
+
+    Future connect () {
+      Completer completer = new Completer();
+
+      if (!this.connected) {
+        List<String> arguments = [this.defaultAccount.username,
+                             this.defaultAccount.password,
+                             this.defaultAccount.server,
+                             this.defaultAccount.SIPPort.toString(),
+                             Configuration.Loglevel.toString()];
+
+
+        IO.Process.start(this.binaryPath , arguments).then((IO.Process process) {
+          this._process = process;
+
+          this._process.exitCode.then((int exitCode) {
+            if (exitCode != 0) {
+              this.log.severe('Process exited with status $exitCode.');
+            }
+            else {
+              this.log.finest('Process exited with status $exitCode.');
+            }
+          });
+
+
+          process.stdout.transform(UTF8.decoder)
+            .transform(new LineSplitter())
+            .listen(this._processOutput);
+
+          process.stderr.transform(UTF8.decoder).transform(new LineSplitter()).listen((String line) {
+            this.log.severe('(pipe, stderr) $line');
+          });
+
+          completer.complete(null);
+        }).catchError((error, stackTrace) => completer.completeError(error,stackTrace));
+      }
+
+      return completer.future;
+    }
+
+    /**
+     * Future returning when the process is ready. Should be used to assert that
+     * the process is ready before sending commands to it.
+     */
+    Future whenReady () {
+      if (this._ready) {
+        return new Future(null);
+      } else {
+        Completer ticket = new Completer();
+        this._readyQueue.add(ticket);
+        return ticket.future.timeout(new Duration(seconds : 20));
+      }
+    }
+
+    void _processOutput (String line) {
+      this.log.fine('(pipe) $line');
+
+      if (['{'].any((char) => line.startsWith(char))) {
+         this._parseAndDispatch(line);
+      }
+
+    }
+
+    void _parseAndDispatch(String line) {
+      Map map = {};
+      try {
+        map = JSON.decode(line);
+      } catch (error) {
+        this.log.severe('Failed to parse line: "$line"');
+      }
+
+      if (map.containsKey('event')) {
+        if(map['event'] == PJSUAEvent.READY) {
+          this._ready = true;
+          while (!this._readyQueue.isEmpty) {
+            this._readyQueue.removeFirst().complete();
+          }
+
+        } else if(map['event'] == PJSUAEvent.OUTGOING_CALL) {
+          //TODO: Extract callID or similar.
+
+          Event event = new CallOutgoing(map['call']['id'].toString(), map['call']['extension']);
+
+          this._eventController.add(event);
+
+        } else if(map['event'] == PJSUAEvent.INCOMING_CALL) {
+          //TODO: Extract callID or similar.
+          Event event = new CallIncoming(map['call']['id'].toString(), map['call']['extension']);
+
+          this._eventController.add(event);
+
+        } else {
+          log.severe('Unknown message: "$line"');
+        }
+
+
+      } else if (map.containsKey('reply')) {
+        this.replyQueue.removeFirst().complete(map['reply']);
+      } else {
+        this.log.severe('Unrecognized line: "$line"');
+      }
+
+    }
+
+    Future _subscribeAndSend (String command) {
+      Completer<String> ticket = new Completer<String>();
+      this.replyQueue.add(ticket);
+
+      this.log.finest('Sending command "$command"');
+      this._process.stdin.writeln(command);
+
+      return ticket.future;
+    }
+
+    Future<int> quitProcess () {
+      log.finest('sending SIGTERM to ${this._process.pid}');
+
+      this._process.kill((IO.ProcessSignal.SIGTERM));
+
+      new Future.delayed(new Duration (seconds: 1), (){
+        log.finest('sending SIGKILL to ${this._process.pid}');
+        this._process.kill((IO.ProcessSignal.SIGKILL));
+      });
+
+      return this._process.exitCode;
+    }
+
+    Future startProcess () {
+      List<String> arguments =  ['1002', '1234', '192.168.1.136', '5071'];
+      return IO.Process.start(this.binaryPath , arguments)
+        .then((IO.Process process) {
+          this._process = process;
+          this._process.stdout
+            .transform (new Utf8Decoder())
+            .transform (new LineSplitter())
+            .listen (this._parseAndDispatch);
+        });
+    }
+
+
+    Future restartProcess () {
+        this.quitProcess();
+        return this.startProcess();
+    }
+
+    Future waitFor (String line, {int timeoutSeconds : 10}) =>
+        new Future (() => throw new StateError('Not implemented!'));
+ }
